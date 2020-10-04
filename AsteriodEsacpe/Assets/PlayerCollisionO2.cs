5b0e100103ef809dd8c71078ecde36569556e329
Eric O'Donoghue <ericodonoghue27@gmail.com>
2020-10-04 15:32:22 -0600

    Collision stuff

--- AsteriodEsacpe/Assets/PlayerCollisionO2.cs
+++ AsteriodEsacpe/Assets/PlayerCollisionO2.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerCollisionO2 : MonoBehaviour
 {
+    public float oxygen = 100.0f;
+
+    public float oxygenLoseRate = 1.0f;
+
+    // TODO: player movemnt script will set this value when jets are being used
+    public float oxygenJetRate = 0.0f;
+
+    public float suitDamage = 1.0f;
+
+    // TODO: player movemnt script will set this value when jets are being used
+    public float fuel = 100.0f;
+
+    public float fuelRate = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // TODO: get values of variables from save data once implemented
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        oxygen -= (oxygenLoseRate * suitDamage) / 50.0f;
+        oxygen -= oxygenJetRate / 50.0f;
+
+        fuel -= fuelRate / 50.0f;
+    }
+
+    private void OnCollisionEnter(Collision c)
+    {
     }
 }
