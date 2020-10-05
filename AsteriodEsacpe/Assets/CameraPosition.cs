22d903a4f7ffe3c9360e5ce0d881f27d25cdc252
PABeckett <parkerabeckett1@gmail.com>
2020-10-04 21:53:54 -0600

    O2/collision/GUI
    Added O2 variables and decrements.
    Added collision logic for caves and future implementation of resources and monster objects (subject to change.)
    Attached camera to player object and created GUI to show oxygen levels as a number and bar (can add fuel easily)

---
+++ AsteriodEsacpe/Assets/CameraPosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+        pos.y = player.transform.position.y + 1f;
+        pos.z = player.transform.position.z - 10f;
+        Camera.main.transform.position = pos;;
+    }
+}
--- AsteriodEsacpe/Assets/PlayerCollisionO2.cs
+++ AsteriodEsacpe/Assets/PlayerCollisionO2.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCollisionO2 : MonoBehaviour
 {
-    public float oxygen = 100.0f;
+    public int timer = 0;
+    Color warn;
+    Color reg;
+    public Text oxygenDisplay;
+    public Slider oxygenBar;
+    public Canvas canvas;
+    public GameObject PlayerModelTest;
+
+    public float oxygen = 99.0f;
 
     public float oxygenLoseRate = 1.0f;
 
@@ -15,7 +24,7 @@ public class PlayerCollisionO2 : MonoBehaviour
     public float suitDamage = 1.0f;
 
     // TODO: player movemnt script will set this value when jets are being used
-    public float fuel = 100.0f;
+    public float fuel = 99.0f;
 
     public float fuelRate = 0.0f;
 
@@ -25,21 +34,76 @@ public class PlayerCollisionO2 : MonoBehaviour
         // TODO: get values of variables from save data once implemented
     }
 
+    private void Awake()
+    {
+        reg = oxygenDisplay.color;
+        Debug.Log(reg);
+        warn = new Color(1 - reg.r, 1 - reg.g, reg.b);
+        Debug.Log(warn);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if(oxygen <= 0)
+        {
+            Destroy(PlayerModelTest);
+        }
+
+        oxygenDisplay.text = (Mathf.RoundToInt(oxygen)).ToString();
+
+        //holding breath?
+
+        //if (oxygen <= -60)
+        //{
+        //    Destroy(PlayerModelTest);
+        //}
     }
 
     void FixedUpdate()
     {
         oxygen -= (oxygenLoseRate * suitDamage) / 50.0f;
         oxygen -= oxygenJetRate / 50.0f;
-
         fuel -= fuelRate / 50.0f;
+        oxygenBar.value = oxygen;
+
+        if (oxygen <= 10)
+        {
+            int oxygenRound = Mathf.RoundToInt(oxygen);
+            if (oxygenRound % 2 == 0)
+                oxygenDisplay.color = warn;
+            else if (oxygenRound % 2 == 1)
+                oxygenDisplay.color = reg;
+        }
+    
+
     }
 
     private void OnCollisionEnter(Collision c)
     {
+        GameObject collided = c.gameObject;
+        if(collided.tag == "Cave")
+        {
+            suitDamage++;
+            //TODO: add impulse to player depending on speed
+        }
+
+        if(collided.tag == "OxygenTank")
+        {
+            if (oxygen <= 74)
+                oxygen += 25;
+            else
+                oxygen = 99;
+        }
+
+        if (collided.tag == "FuelTank")
+        {
+            if (fuel <= 74)
+                fuel += 25;
+            else
+                fuel = 99;
+        }
+        if (collided.tag == "Monster")
+            Destroy(PlayerModelTest);
     }
 }
