1e6f593c3ed8c802ebbcb9185a72b7e5f97e0a37
Eric O'Donoghue <ericodonoghue27@gmail.com>
2020-10-18 10:59:03 -0600

    added tanks

---
+++ AsteriodEsacpe/Assets/Scripts/O2TankMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class O2TankMovement : MonoBehaviour
+{
+    public Rigidbody tankRB;
+    readonly float force = 8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tankRB = GetComponent<Rigidbody>();
+
+        tankRB.AddRelativeForce(new Vector3(Random.Range(0f, 2f) * force, Random.Range(0f, 2f) * force, Random.Range(0, 2f) * force));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tankRB.AddRelativeForce(new Vector3(Random.Range(0f, 2f) * force, Random.Range(0f, 2f) * force, Random.Range(0, 2f) * force));
+    }
+}
