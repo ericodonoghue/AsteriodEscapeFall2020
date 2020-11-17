d6beac5e2920f0026975ef88423f5eeca9352ff5
Eric O'Donoghue <ericodonoghue27@gmail.com>
2020-11-16 18:57:40 -0700

    WinState

---
+++ AsteriodEsacpe/Assets/Scripts/YouWinControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YouWinControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //TODO: you win stuff
+        Time.timeScale = 0;
+        Debug.Log("You Win");
+        SceneManager.LoadScene("LevelSelectScene");
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+}
