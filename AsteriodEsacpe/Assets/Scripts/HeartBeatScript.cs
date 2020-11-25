8c03a0bc3054179583cbb250b99520f7a7b76eb2
Eric O'Donoghue <ericodonoghue27@gmail.com>
2020-11-24 19:47:41 -0700

    Intial set up and work for heart beat speed increase

---
+++ AsteriodEsacpe/Assets/Scripts/HeartBeatScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartBeatScript : MonoBehaviour
+{
+
+    public AudioSource heartBeat;
+
+    public float intialDelay = 1.0f;
+
+    private float delay = 1.0f;
+
+    private float cdValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCountdown(intialDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private IEnumerator StartCountdown(float countdownValue)
+    {
+        cdValue = countdownValue;
+        while (cdValue > 0)
+        {
+            yield return new WaitForSeconds(0.1f);
+            cdValue -= 0.1f;
+        }
+        heartBeat.Play();
+
+        // TODO: calculate the delay change based on current player heartrate / O2
+        StartCountdown(delay);
+    }
+}
