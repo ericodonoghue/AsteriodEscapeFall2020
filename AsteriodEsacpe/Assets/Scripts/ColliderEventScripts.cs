a6244e7dceaa877a8176f9aa84c8f73d9a7e8ebd
Eric O'Donoghue <ericodonoghue27@gmail.com>
2020-12-08 08:52:36 -0700

    Trigger work for sound effects and lights turning off

---
+++ AsteriodEsacpe/Assets/Scripts/ColliderEventScripts.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderEventScripts : MonoBehaviour
+{
+    public AudioSource scarySound1;
+
+    private GameObject lights;
+
+    private float cdValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lights = GameObject.FindGameObjectWithTag("CaveLights");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        GameObject collided = c.gameObject;
+
+        if (collided.tag == "ScarySound1")
+        {
+            scarySound1.Play();
+        }
+        else if (collided.tag == "LightFlicker1")
+        {
+            lights.SetActive(false);
+            StartCoroutine(StartCountdown());
+        }
+    }
+
+    private IEnumerator StartCountdown(float countdownValue = 3)
+    {
+        cdValue = countdownValue;
+        while (cdValue > 0)
+        {
+            UnityEngine.Debug.Log("Countdown: " + cdValue);
+            yield return new WaitForSeconds(1.0f);
+            cdValue--;
+        }
+        lights.SetActive(true);
+    }
+
+}
