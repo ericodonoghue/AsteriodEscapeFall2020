2bfda48c721bef37bd8570d1ff22a7ce0980e33e
PABeckett <parkerabeckett1@gmail.com>
2020-12-12 17:42:29 -0700

    better UI intermediate

--- AsteriodEsacpe/Assets/Scripts/HeadsUpDisplay.cs
+++ AsteriodEsacpe/Assets/Scripts/HeadsUpDisplay.cs
@@ -7,14 +7,14 @@ public class HeadsUpDisplay : MonoBehaviour
   // Local reference to the central AvatarAccounting object (held by main camera)
   private AvatarAccounting avatarAccounting;
 
-  private Color warnOxygen;
-  private Color regOxygen;
+  private Color warnOxygen;//
+  private Color regOxygen;//
 
 
   private void Awake()
   {
     Text valueLabelText = GameObject.Find("OxygenTankValue").GetComponent<Text>();
-    regOxygen = valueLabelText.color;
+    regOxygen = valueLabelText.color;//cyan
     warnOxygen = new Color(1 - regOxygen.r, 1 - regOxygen.g, regOxygen.b);
   }
 
@@ -36,16 +36,17 @@ public class HeadsUpDisplay : MonoBehaviour
   private void UpdateHUD()
   {
     // Set UX guages
-    SetUXData("HeartRate", avatarAccounting.CurrentHeartRatePerMinute, " bpm");
-    SetUXData("RespirationRate", avatarAccounting.CurrentRespirationRatePerMinute, " bpm");
-    SetUXData("JetBurnRate", avatarAccounting.CurrentJetBurnRatePerSecond, " ml\\s");
-    SetUXData("SuitIntegrity", avatarAccounting.CurrentSuitIntegrityInPercentage, " units");
-    SetUXData("OxygenBurnRate", avatarAccounting.CurrentOxygenBurnRatePerSecond, " ml\\s");
-    SetUXData("OxygenTank", avatarAccounting.CurrentOxygenAllTanksContent, " litres");
-    SetUXData("BloodOxygen", avatarAccounting.CurrentBloodOxygenPercent, "%");
-  }
+    //SetUXData("HeartRate", avatarAccounting.CurrentHeartRatePerMinute, " bpm");
+    //SetUXData("RespirationRate", avatarAccounting.CurrentRespirationRatePerMinute, " bpm");
+    //SetUXData("JetBurnRate", avatarAccounting.CurrentJetBurnRatePerSecond, " ml\\s");
+    SetUXData("SuitIntegrity", avatarAccounting.CurrentSuitIntegrityInPercentage, "%");
+    //SetUXData("OxygenBurnRate", avatarAccounting.CurrentOxygenBurnRatePerSecond, " ml\\s");
+    SetUXData("OxygenTank", avatarAccounting.CurrentOxygenAllTanksContent, " L");//o2 tanks instead
+    // SetUXData("BloodOxygen", avatarAccounting.CurrentBloodOxygenPercent, "%");
+    //SetUXData("PonyBottle", avatarAccounting.CurrentOxygenPonyBottleContent, " litres");
+    }
 
-  private void SetUXData(string gameObjectNameBase, float value, string tag = "")
+  private void SetUXData(string gameObjectNameBase, float value, string tag = "")//pony bottle tank, slider, value
   {
     // use gameObjectNameBase to create actual object names
     string valueLabelName = gameObjectNameBase + "Value";
